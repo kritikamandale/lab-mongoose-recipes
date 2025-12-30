736f91085b0c31d26767aaf31e93611dd676f9ac
kritikamandale <kritikamandale1421@gmail.com>
2025-12-30 11:35:19 +0530

    task done

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
+const data = require("./data.json");
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
-
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+mongoose.connect("mongodb+srv://kritikamandale_db_user:I9PosQPG2jWEJ7fw@cluster0.6eccz9c.mongodb.net/recipie_app")
+  .then(() => {
+    console.log("Connected to DB");
+    
+    // Delete existing recipe if it exists
+    return Recipe.deleteOne({title:"Asian Glazed Chicken Thighs"});
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log("Cleared any existing recipe");
+
+    return Recipe.create({
+      title: "Asian Glazed Chicken Thighs",
+      level: "intermidiate",
+      ingrediants: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      cusine: "Asian",
+      dishtype: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu"
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .then(recipe => {
+    console.log(`Recipe created: ${recipe.title}`);
+    
+    return Recipe.findOneAndUpdate(
+      {title:"Asian Glazed Chicken Thighs"},
+      {duration: 100},
+      {new:true}
+    );
+  })
+  .then((updatedRecipe) => {
+    console.log("recipe duration updated successfully");
+    
+    return Recipe.findOneAndUpdate(
+      {title:"Asian Glazed Chicken Thighs"},
+      {dishtype: "soup"},
+      {new:true}
+    );
+  })
+  .then(() => {
+    console.log("dishtype updated successfully");
+    
+    return Recipe.deleteOne({title:"Asian Glazed Chicken Thighs"});
+  })
+  .then(() => {
+    console.log("recipe deleted successfully");
+  })
+.catch(err => console.error(err))
+.finally(() => {
+  mongoose.connection.close();
+});
